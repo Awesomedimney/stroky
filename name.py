c52487f25d4bd0578343c71776f20aaeffcae19b
Awesomedimney <dimas7kom@gmail.com>
2025-01-13 04:39:21 +0300

    name.py

--- name.py
+++ name.py
@@ -13,7 +13,7 @@ def is_contains(string, list):
 
 print(string_info('Capybara'))
 print(string_info('Armageddon'))
-print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))  # Urban ~ urBAN
-print(is_contains('cycle', ['recycling', 'cyclic']))    # No matches
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))
+print(is_contains('cycle', ['recycling', 'cyclic']))
 
 print(calls)
